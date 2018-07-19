21667d17fd53c948b7c38eb4b34e6186e5bb6579
Aaron Lou <al968@cornell.edu>
2018-07-18 17:37:47 -0700

    Basic Framework built

---
+++ replay_buffer.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+class replay_buffer(object):
+    def __init__(self):
+        pass
+    def add(self, tuple):
+        pass
+    def sample(self):
+        pass
---
+++ train_gan_q_learning.py
@@ -0,0 +1,13 @@
+import numpy as numpy
+import tensorflow as tf
+
+def learn(env,
+        q_func,
+        discriminator,
+        generator,
+        learning_rate,
+        n_disc,
+        n_gen,
+        lambda_,
+        batch_size):
+    pass
