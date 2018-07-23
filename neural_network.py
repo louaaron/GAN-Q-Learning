99d67041cc778f7c93fca83bf97c35ac700d5186
Aaron Lou <al968@cornell.edu>
2018-07-22 20:16:01 -0700

    Framework for neural network

---
+++ neural_network.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np 
+
+class Model(object):
+    @property
+    def input_ph(self):
+        pass
+
+    @property
+    def output(self):
+        pass
+
+    @property
+    def sess(self):
+        pass
